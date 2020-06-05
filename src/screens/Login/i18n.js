6c9a79ce50c5177452ef3724adc87d4705efe6d1
Henry Zarza <henryacz7@gmail.com>
2020-06-05 17:59:08 -0500

    Login screen layout

--- src/screens/Login/i18n.js
+++ src/screens/Login/i18n.js
@@ -1,5 +1,10 @@
 import i18next from 'i18next';
 
 i18next.addResources('en', 'LOGIN', {
-  TITLE: 'Login',
+  TITLE: 'Graph countries',
+  SUBTITLE: 'A React training app using a GraphQL API to query country-related',
+  EMAIL: 'Email',
+  PASSWORD: 'Password',
+  CREATE_ACCOUNT: "Don't have an account?",
+  BTN_LOGIN: 'Log In',
 });
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import i18next from 'i18next';
+import Input from '@components/Input';
+import styles from './styles.module.scss';
 
 const Login = () => {
   return (
-    <section>
-      <h2 className='title'>{i18next.t('LOGIN:TITLE')}</h2>
-      <p className='base-text'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit veritatis
-        dolore neque optio excepturi non, sequi numquam, maxime quidem
-        aspernatur voluptate architecto? Molestias, officiis magni cumque facere
-        aut animi quidem.
-      </p>
+    <section className={styles.container}>
+      <section className={styles.leftContainer}>
+        <form className={styles.leftContent}>
+          <h2 className={`title ${styles.mb1}`}>{i18next.t('LOGIN:TITLE')}</h2>
+          <p className={`base-text ${styles.mb1}`}>
+            {i18next.t('LOGIN:SUBTITLE')}
+          </p>
+          <Input id='email' label={i18next.t('LOGIN:EMAIL')} />
+          <Input
+            id='password'
+            label={i18next.t('LOGIN:PASSWORD')}
+            type='password'
+          />
+          <button
+            type='button'
+            className={`button primary base-text fw-bold ${styles.mb1}`}
+          >
+            {i18next.t('LOGIN:BTN_LOGIN')}
+          </button>
+          <button
+            type='button'
+            className={`small-text fw-bold link ${styles.link}`}
+          >
+            {i18next.t('LOGIN:CREATE_ACCOUNT')}
+          </button>
+        </form>
+      </section>
+      <section className={styles.rightContainer} />
     </section>
   );
 };
