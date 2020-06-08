38f74df8f6c44bd5035ef92780ab8ee3245034b6
Henry Zarza <henryacz7@gmail.com>
2020-06-08 17:28:05 -0500

    Sign in screen and toggle swith between screens

--- src/screens/Login/i18n.js
+++ src/screens/Login/i18n.js
@@ -5,6 +5,11 @@ i18next.addResources('en', 'LOGIN', {
   SUBTITLE: 'A React training app using a GraphQL API to query country-related',
   EMAIL: 'Email',
   PASSWORD: 'Password',
+  NAME: 'Name',
+  USER_NAME: 'User name',
+  PASSWORD_REPEAT: 'Repeat password',
   CREATE_ACCOUNT: "Don't have an account?",
   BTN_LOGIN: 'Log In',
+  BTN_SIGN_IN: 'Sign In',
+  LINK_LOGIN: 'I have an account',
 });
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,36 +1,24 @@
-import React from 'react';
-import i18next from 'i18next';
-import Input from '@components/Input';
+import React, { useCallback, useState } from 'react';
+import LoginForm from './components/LoginForm';
+import SignUpForm from './components/SignUpForm';
 import styles from './styles.module.scss';
 
 const Login = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const handleChangeView = useCallback(() => {
+    setIsLogin(!isLogin);
+  }, [isLogin, setIsLogin]);
+
+  const Screen = isLogin ? LoginForm : SignUpForm;
+
   return (
     <section className={styles.container}>
       <section className={styles.leftContainer}>
-        <form className={styles.leftContent}>
-          <h2 className={`title ${styles.mb1}`}>{i18next.t('LOGIN:TITLE')}</h2>
-          <p className={`base-text ${styles.mb1}`}>
-            {i18next.t('LOGIN:SUBTITLE')}
-          </p>
-          <Input id='email' label={i18next.t('LOGIN:EMAIL')} />
-          <Input
-            id='password'
-            label={i18next.t('LOGIN:PASSWORD')}
-            type='password'
-          />
-          <button
-            type='button'
-            className={`button primary base-text fw-bold ${styles.mb1}`}
-          >
-            {i18next.t('LOGIN:BTN_LOGIN')}
-          </button>
-          <button
-            type='button'
-            className={`small-text fw-bold link ${styles.link}`}
-          >
-            {i18next.t('LOGIN:CREATE_ACCOUNT')}
-          </button>
-        </form>
+        <Screen
+          className={styles.leftContent}
+          onChangeView={handleChangeView}
+        />
       </section>
       <section className={styles.rightContainer} />
     </section>
