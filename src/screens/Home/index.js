0c09bce5307fc4655cd6e9b0419ad0d9205a181e
Henry Zarza <henryacz7@gmail.com>
2020-06-12 13:52:14 -0500

    Logout logic

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useContext, useCallback } from 'react';
 import i18next from 'i18next';
 
+import { AuthContext } from '@components/ProtectedRoute';
 import logo from '@assets/logo.svg';
 import styles from './styles.module.scss';
 
 function Home() {
+  const context = useContext(AuthContext);
+
+  const handleLogout = useCallback(() => context.handleSetIsAuth(false), [
+    context,
+  ]);
+
   return (
     <div className={styles.app}>
       <header className={styles.appHeader}>
         <img src={logo} className={styles.appLogo} alt='logo' />
         <h2 className='title'>{i18next.t('HOME:HOME_TITLE')}</h2>
         <p className='base-text'>{i18next.t('HOME:HOME_DESCRIPTION')}</p>
-        <a
-          className={`big-text ${styles.appLink}`}
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+        <button
+          type='button'
+          className='button primary base-text fw-bold m-bottom-4'
+          onClick={handleLogout}
         >
           {i18next.t('HOME:BUTTON_TEXT')}
-        </a>
+        </button>
       </header>
     </div>
   );
