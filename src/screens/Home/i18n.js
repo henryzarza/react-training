24fbc9508211ac929f924d22713334aed7a3060a
Henry Zarza <henryacz7@gmail.com>
2020-06-13 16:25:50 -0500

    Add  i18n to loading component

--- src/screens/Home/i18n.js
+++ src/screens/Home/i18n.js
@@ -5,4 +5,5 @@ i18next.addResources('en', 'HOME', {
   HOME_DESCRIPTION:
     "It's time for a killer workout with trainers who can see when you make a mistake and help you fix it. Live.",
   BUTTON_TEXT: 'Learn more ...',
+  LOADING: 'Loading',
 });
