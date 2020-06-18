5df5cb01c687ed21fa8ecf389be69b364513a9d6
Henry Zarza <henryacz7@gmail.com>
2020-06-18 17:48:46 -0500

    General structure and layout

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,32 +1,63 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import i18next from 'i18next';
+import { Link } from '@reach/router';
 
+import { NAVBAR_ROUTES } from '../../constants/routes';
 import styles from './styles.module.scss';
 
 function Navbar() {
+  const isLinkActive = useCallback(
+    ({ isCurrent }) =>
+      // eslint-disable-next-line no-useless-computed-key
+      isCurrent ? { ['data-active']: true } : {},
+    []
+  );
+
   return (
-    <section className={styles.navbarContainer}>
+    <section className={styles.container}>
       <div className={styles.content}>
-        <h2 className='subtitle m-bottom-3'>{i18next.t('LOGIN:TITLE')}</h2>
-        <p className='base-text m-bottom-1'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis a
-          harum vel distinctio nobis fuga unde. Dolorem, facilis earum? Numquam,
-          corrupti officiis? Corrupti, obcaecati voluptatem cumque quidem quam
-          enim voluptatibus?
+        <h2 className={`title m-bottom-3 ${styles.title}`}>
+          {i18next.t('LOGIN:TITLE')}
+        </h2>
+        <p className={`base-text m-bottom-6 ${styles.paragraph}`}>
+          A simple application using React (with hooks, portals, context, and
+          between other interesting things), this app is just a demo becasue I
+          wanna learn to connect GraphQL APIs to React apps.
         </p>
         <a
           rel='nofollow external'
           target='blank'
           href='https://github.com/lennertVanSever/graphcountries'
-          className='small-text link m-bottom-1'
+          className='small-text link m-bottom-2 fw-bold'
         >
-          lennertVanSever/graphcountries repository
+          Repository: lennertVanSever/graphcountries
         </a>
         <small className='small-text m-bottom-1'>
-          Made with love ♥️ by Henry Zarza
+          Made with love
+          <span className='m-left-1' role='img' aria-label='love'>
+            ♥️
+          </span>
+          by Henry Zarza
         </small>
       </div>
-      <nav className={styles.navbar}></nav>
+      <nav className={styles.navbar}>
+        {NAVBAR_ROUTES.map((el) => (
+          <Link
+            key={el.route}
+            to={el.route}
+            className={`big-text fw-thin uppercase-text text-center ${styles.navbarItem}`}
+            getProps={isLinkActive}
+          >
+            {el.text}
+          </Link>
+        ))}
+        <button
+          type='button'
+          className={`big-text fw-thin uppercase-text text-center ${styles.navbarItem}`}
+        >
+          Logout
+        </button>
+      </nav>
     </section>
   );
 }
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,22 @@
 export const ROUTES = {
   HOME: '/',
   TIME_ZONES: 'time-zones',
+  INTERESTING: 'interesting',
 };
 
 export const IS_AUTH = 'isAuth';
+
+export const NAVBAR_ROUTES = [
+  {
+    route: ROUTES.HOME,
+    text: 'Home',
+  },
+  {
+    route: ROUTES.TIME_ZONES,
+    text: 'Time zones',
+  },
+  {
+    route: ROUTES.INTERESTING,
+    text: 'Interesting',
+  },
+];
