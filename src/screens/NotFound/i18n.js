407bf3d8c6a4d1c70e63ac5324a662d4c18ecb7c
Henry Zarza <henryacz7@gmail.com>
2020-06-23 17:02:14 -0500

    404 layout page

---
+++ src/screens/NotFound/i18n.js
@@ -0,0 +1,7 @@
+import i18next from 'i18next';
+
+i18next.addResources('en', 'NOT_FOUND', {
+  TITLE: 'Page not found',
+  MESSAGE: "Sorry but the page you are looking for doesn't exist.",
+  BTN_COME_BACK: 'Come back home',
+});
--- src/screens/NotFound/index.js
+++ src/screens/NotFound/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import i18next from 'i18next';
+import { Link } from '@reach/router';
+
+import { ROUTES } from '@constants/routes';
+import styles from './styles.module.scss';
 
 function NotFound() {
-  return <h2 className='title'>Not Found - 404</h2>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.img404}>404</div>
+      <h2 className={`title m-bottom-3 ${styles.textShadow}`}>
+        {i18next.t('NOT_FOUND:TITLE')}
+      </h2>
+      <p className='base-text m-bottom-5'>{i18next.t('NOT_FOUND:MESSAGE')}</p>
+      <Link to={ROUTES.HOME} className='button primary'>
+        {i18next.t('NOT_FOUND:BTN_COME_BACK')}
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
