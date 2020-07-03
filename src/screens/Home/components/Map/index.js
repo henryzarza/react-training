3ff54cf9ea5b774680d3e37611ce660fe2997552
Henry Zarza <henryacz7@gmail.com>
2020-07-03 17:06:08 -0500

    Map layout and configuration

--- src/screens/Home/components/Map/index.js
+++ src/screens/Home/components/Map/index.js
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { string, shape, number, arrayOf } from 'prop-types';
+import { create, color } from '@amcharts/amcharts4/core';
+import {
+  MapChart,
+  projections,
+  MapPolygonSeries,
+} from '@amcharts/amcharts4/maps';
+import am4geodata_worldLow from '@amcharts/amcharts4-geodata/worldLow';
 
-function Map() {
-  return (
-    <div>
-      <h5 className='big-text'>Map view</h5>
-      <p className='base-text'>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas eum
-        numquam aut quo accusamus odio, sunt suscipit eos iure ducimus quod
-        delectus, deserunt minus. Ipsam unde voluptas officia aliquam nemo.
-        Beatae corrupti asperiores ex hic ea totam tempore natus, perferendis
-        labore, voluptatum officia laboriosam dignissimos minima soluta tenetur
-        harum repellat optio cumque quo laudantium aperiam mollitia! Nihil sequi
-        officia asperiores? Aspernatur, tempore quod. Unde quasi esse animi quo
-        amet culpa pariatur cupiditate, molestiae expedita, exercitationem, sunt
-        eveniet optio! Quo, tenetur esse. Iure perspiciatis atque non tempore
-        veritatis similique ipsum dolore.
-      </p>
-    </div>
-  );
+import { THEME_COLORS } from '@constants/colors';
+import styles from './styles.module.scss';
+
+function Map({ data }) {
+  useEffect(() => {
+    let map = create('mapRef', MapChart);
+
+    map.geodata = am4geodata_worldLow;
+    map.projection = new projections.Miller();
+    let polygonSeries = map.series.push(new MapPolygonSeries());
+    polygonSeries.useGeodata = true;
+    let polygonTemplate = polygonSeries.mapPolygons.template;
+    polygonTemplate.tooltipText = '{emoji} {name} ({nativeName})';
+    polygonTemplate.fill = color(THEME_COLORS.DEFAULT.primary);
+    let hs = polygonTemplate.states.create('hover');
+    hs.properties.fill = color(THEME_COLORS.DEFAULT.secondary);
+
+    polygonSeries.exclude = ['AQ'];
+    polygonSeries.data = data;
+
+    polygonTemplate.events.on('hit', function (e) {
+      e.target.series.chart.zoomToMapObject(e.target);
+      console.table(e.target.dataItem.dataContext);
+    });
+
+    return () => {
+      if (map) {
+        map.dispose();
+      }
+    };
+  }, [data]);
+
+  return <div className={styles.container} id='mapRef' />;
 }
+
+Map.propTypes = {
+  data: arrayOf(
+    shape({
+      id: string,
+      name: string,
+      nativeName: string,
+      externalId: number,
+      emoji: string,
+    }).isRequired
+  ).isRequired,
+};
 
 export default Map;
