532c0971935569fa47bd9918586c3c8af8e553be
Henry Zarza <henryacz7@gmail.com>
2020-07-11 19:19:53 -0500

    Tooltip component starter files

---
+++ src/screens/Interesting/components/Tooltip/index.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+// import { string, shape } from 'prop-types';
+import clsx from 'clsx';
+import i18next from 'i18next';
+
+import styles from './styles.module.scss';
+
+function Tooltip({ coordinates }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    console.log('coordinates', coordinates);
+    if (coordinates) {
+      setIsVisible(true);
+      contentRef.current.style.transform = `translate(${coordinates.x}, ${coordinates.y})`;
+    }
+  }, [coordinates]);
+
+  const handleClick = useCallback(() => {
+    setIsVisible(false);
+  }, []);
+
+  return (
+    <div
+      ref={contentRef}
+      className={clsx(styles.content, { [styles.visible]: isVisible })}
+      onClick={handleClick}
+    >
+      <div className={styles.innerContent}>
+        <h3 className='big-text m-bottom-2 fw-bold'>
+          {/* {data.name}({data.symbol}) */}
+          Euro (€)
+        </h3>
+        <h6 className='base-text m-bottom-2'>
+          {i18next.t('INTERESTING:CURRENCY_COUNTRIES')}
+        </h6>
+        <ul className={styles.countries}>
+          <li className={`small-text m-bottom-1 ${styles.item}`}>
+            <span role='img' aria-label='Zimbabwe'>
+              🇿🇼
+            </span>
+            Zimbabwe (Zimbabwe)
+          </li>
+          <li className={`small-text m-bottom-1 ${styles.item}`}>
+            <span role='img' aria-label='Zimbabwe'>
+              🇿🇼
+            </span>
+            Zimbabwe(Zimbabwe)
+          </li>
+          <li className={`small-text m-bottom-1 ${styles.item}`}>
+            <span role='img' aria-label='Zimbabwe'>
+              🇿🇼
+            </span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            minima nobis iure necessitatibus quae iusto molestiae non, eum nulla
+            sapiente. Laudantium tenetur enim eum nisi repudiandae ex porro
+            ipsum aut!
+          </li>
+          <li className={`small-text m-bottom-1 ${styles.item}`}>
+            <span role='img' aria-label='Zimbabwe'>
+              🇿🇼
+            </span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            minima nobis iure necessitatibus quae iusto molestiae non, eum nulla
+            sapiente. Laudantium tenetur enim eum nisi repudiandae ex porro
+            ipsum aut!
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+/* Tooltip.propTypes = {
+  data: shape({
+    _id: string,
+    name: string,
+    code: string,
+    symbol: string,
+  }).isRequired,
+}; */
+
+export default Tooltip;
--- src/screens/Interesting/i18n.js
+++ src/screens/Interesting/i18n.js
@@ -4,4 +4,5 @@ i18next.addResources('en', 'INTERESTING', {
   TAB_CURRENCY: 'Currencies',
   TAB_LANGUAGES: 'Languages',
   TAB_DISTANCE: 'Distance',
+  CURRENCY_COUNTRIES: 'Countries:',
 });
