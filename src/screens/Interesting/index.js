a2d59646a122ebc91a1909f721b59f0b7744464a
Henry Zarza <henryacz7@gmail.com>
2020-07-23 16:05:28 -0500

    Delete unnecessary code

--- src/screens/Interesting/index.js
+++ src/screens/Interesting/index.js
@@ -1,30 +1,19 @@
 import React, { useState } from 'react';
 
 import Tabs from './components/Tabs';
-import Coin from './components/Coin';
+import CurrencyScreen from './components/CurrencyScreen';
 import Language from './components/Language';
 import styles from './styles.module.scss';
-import {
-  TABS_CONFIG,
-  MOCK_CURRENCY_DATA,
-  MOCK_LANGUAGES_DATA,
-} from './constants';
+import { TABS_CONFIG, MOCK_LANGUAGES_DATA } from './constants';
 
 function Interesting() {
   const [viewType, setViewType] = useState(TABS_CONFIG[0].id);
-  const [currencies] = useState(MOCK_CURRENCY_DATA);
   const [languages] = useState(MOCK_LANGUAGES_DATA);
 
   return (
     <section className={styles.container}>
       <Tabs value={viewType} onSelected={setViewType} />
-      {viewType === TABS_CONFIG[0].id && (
-        <div className={styles.currencyContainer}>
-          {currencies.map((el) => (
-            <Coin key={el._id} data={el} />
-          ))}
-        </div>
-      )}
+      {viewType === TABS_CONFIG[0].id && <CurrencyScreen />}
       {viewType === TABS_CONFIG[1].id && (
         <div className={styles.languageContainer}>
           {languages.map((el) => (
