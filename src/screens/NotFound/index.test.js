a6f9142b261a6200782fe6e31edd2a660f6651a2
Henry Zarza <henryacz7@gmail.com>
2021-01-11 13:11:44 -0500

    NotFound screen test

--- src/screens/NotFound/index.test.js
+++ src/screens/NotFound/index.test.js
@@ -5,4 +5,6 @@ import NotFound from './index';
 test('Renders NotFound screen', () => {
   const { getByText } = render(<NotFound />);
   expect(getByText('404')).toBeInTheDocument();
+  expect(getByText('NOT_FOUND:TITLE')).toBeInTheDocument();
+  expect(getByText('NOT_FOUND:MESSAGE')).toBeInTheDocument();
 });
