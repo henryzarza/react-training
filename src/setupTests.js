84ef9c6c8bc7aa05b1e5d14d5de6c2c0f7ed9f38
Henry Zarza <henryacz7@gmail.com>
2021-01-12 09:25:56 -0500

    Setup test

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
+import 'mutationobserver-shim';
+
+jest.mock('i18next', () => ({
+  t: (key, params) => (params ? `${key} ${JSON.stringify(params)}` : key)
+}));
