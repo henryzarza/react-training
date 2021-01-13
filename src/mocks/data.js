f942891adc5c7411d08fa58e0fe145f2fbe0ec8d
Henry Zarza <henryacz7@gmail.com>
2021-01-13 15:39:00 -0500

    Mocks and msw configuration

---
+++ src/mocks/data.js
@@ -0,0 +1,121 @@
+export const MOCK_CURRENCIES = {
+  "data": {
+    "Currency": [
+      {
+        "_id": "25",
+        "name": "Afghan afghani",
+        "code": "AFN",
+        "symbol": "؋",
+        "countries": [
+          {
+            "name": "Afghanistan",
+            "flag": {
+              "emoji": "🇦🇫"
+            }
+          }
+        ]
+      },
+      {
+        "_id": "72",
+        "name": "Albanian lek",
+        "code": "ALL",
+        "symbol": "L",
+        "countries": [
+          {
+            "name": "Albania",
+            "flag": {
+              "emoji": "🇦🇱"
+            }
+          }
+        ]
+      },
+      {
+        "_id": "94",
+        "name": "Algerian dinar",
+        "code": "DZD",
+        "symbol": "د.ج",
+        "countries": [
+          {
+            "name": "Algeria",
+            "flag": {
+              "emoji": "🇩🇿"
+            }
+          },
+          {
+            "name": "Western Sahara",
+            "flag": {
+              "emoji": "🇪🇭"
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
+
+export const MOCK_COUNTRIES = {
+  "data": {
+    "Country": [
+      {
+        "_id": "3",
+        "name": "Afghanistan",
+        "nativeName": "افغانستان",
+        "alpha3Code": "AFG",
+        "alpha2Code": "AF",
+        "capital": "Kabul",
+        "population": 27657145,
+        "numericCode": "004",
+        "currencies": [
+          {
+            "name": "Afghan afghani",
+            "symbol": "؋"
+          }
+        ],
+        "flag": {
+          "svgFile": "https://restcountries.eu/data/afg.svg",
+          "emoji": "🇦🇫"
+        }
+      },
+      {
+        "_id": "51",
+        "name": "Albania",
+        "nativeName": "Shqipëria",
+        "alpha3Code": "ALB",
+        "alpha2Code": "AL",
+        "capital": "Tirana",
+        "population": 2886026,
+        "numericCode": "008",
+        "currencies": [
+          {
+            "name": "Albanian lek",
+            "symbol": "L"
+          }
+        ],
+        "flag": {
+          "svgFile": "https://restcountries.eu/data/alb.svg",
+          "emoji": "🇦🇱"
+        }
+      },
+      {
+        "_id": "74",
+        "name": "Algeria",
+        "nativeName": "الجزائر",
+        "alpha3Code": "DZA",
+        "alpha2Code": "DZ",
+        "capital": "Algiers",
+        "population": 40400000,
+        "numericCode": "012",
+        "currencies": [
+          {
+            "name": "Algerian dinar",
+            "symbol": "د.ج"
+          }
+        ],
+        "flag": {
+          "svgFile": "https://restcountries.eu/data/dza.svg",
+          "emoji": "🇩🇿"
+        }
+      }
+    ]
+  }
+};
